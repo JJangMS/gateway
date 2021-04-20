df613e1627693de216e59f16fe8c988041807aef
장민수 <dmsb0120@naver.com>
2021-04-20 17:43:12 +0900

    RouteLocator Test

--- src/main/java/com/jjangms/gateway/GatewayApplication.java
+++ src/main/java/com/jjangms/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.jjangms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,25 @@ public class GatewayApplication {
     SpringApplication.run(GatewayApplication.class, args);
   }
 
+  @Bean
+  RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+    return routeLocatorBuilder
+        .routes()
+        .route(routeSpect -> routeSpect
+            .path("/hello")
+            .and()
+            .host("*.spring.io")
+            .filters(gatewayFilterSpec ->
+                gatewayFilterSpec.setPath("/guides"))
+            .uri("https://spring.io/")
+        )
+        .route("twitter", routeSpect -> routeSpect
+            .path("/twitter/@**")
+            .filters(filters -> filters.rewritePath(
+                "/twiiter/(?<handle>.*)", "/${handle}"
+            ))
+            .uri("https://twitter.com/@")
+        )
+        .build();
+  }
 }
